200a302a56bae787316b7674fb2c09dfc49c1367
Yiheng Xiong <yiheng.xiong@tum.de>
2022-10-24 12:05:35 +0200

    no output jpeg and depthbuffer for per frame

--- pointCloudSample/ARReceiver.swift
+++ pointCloudSample/ARReceiver.swift
@@ -10,6 +10,7 @@ import SwiftUI
 import Combine
 import ARKit
 import AVFoundation
+import CoreMotion
 
 struct VideoSettings {
     var size = CGSize(width: 1920, height: 1440) // predefined in arkit
@@ -60,6 +61,8 @@ final class ARReceiver: NSObject, ARSessionDelegate {
     var videoWriter: VideoWriter?
     var depthBufferSequence: Data?
     
+    var motion = CMMotionManager()
+    
     // Configure and start the ARSession.
     override init() {
         super.init()
@@ -88,7 +91,9 @@ final class ARReceiver: NSObject, ARSessionDelegate {
             self.settings.videoFilename = directory
             self.videoWriter = VideoWriter(videoSettings: self.settings)
             self.videoWriter!.start()
+            self.motion.startDeviceMotionUpdates()
         } else {
+            self.motion.stopDeviceMotionUpdates()
             self.videoWriter!.videoWriterInput.markAsFinished()
             self.videoWriter!.videoWriter.finishWriting {
                 print("finish video generating")
@@ -125,6 +130,7 @@ final class ARReceiver: NSObject, ARSessionDelegate {
             arData.uiImageColor = uiImageColor
             
             if self.isRecord {
+                let fileName = "" + arData.timeStamp.description
                 CVPixelBufferLockBaseAddress(arData.depthImage!, CVPixelBufferLockFlags(rawValue: 0))
                 let depthAddr = CVPixelBufferGetBaseAddress(arData.depthImage!)
                 let depthHeight = CVPixelBufferGetHeight(arData.depthImage!)
@@ -139,13 +145,11 @@ final class ARReceiver: NSObject, ARSessionDelegate {
                     fatalError("addBuffer() failed")
                 }
                 
-                // save a binary stream
+                // save a binary sequence
                 if self.frameNum == 0 {
                     self.depthBufferSequence = depthBuffer
                 } else {
-                    do {
-                        self.depthBufferSequence?.append(depthBuffer)
-                    }
+                    self.depthBufferSequence?.append(depthBuffer)
                 }
                 
                 self.frameNum += 1
@@ -155,24 +159,35 @@ final class ARReceiver: NSObject, ARSessionDelegate {
                 let exposureDuration = "" + arData.exposureDuration.description
                 let exposureOffset = "" + arData.exposureOffset.description
                 
-                let fileName = "" + arData.timeStamp.description
+                // imu data
+                if let data = self.motion.deviceMotion {
+                    let imu = NSArray(array: [data.rotationRate.x, data.rotationRate.y, data.rotationRate.z, data.userAcceleration.x, data.userAcceleration.y, data.userAcceleration.z, data.magneticField.field.x, data.magneticField.field.y, data.magneticField.field.z, data.attitude.roll, data.attitude.pitch, data.attitude.yaw, data.gravity.x, data.gravity.y, data.gravity.z])
+                    // imu data's timestamp is different from arkit's
+                    let motionFileName = "" + data.timestamp.description
+                    if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+                        let imuURL = dir.appendingPathComponent(self.directory + "/" + motionFileName + "_imu.xml")
+                        (imu as NSArray).write(to: imuURL, atomically: false)
+                    }
+                }
                 
                 if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
                     let intriURL = dir.appendingPathComponent(self.directory + "/intri.xml")
                     let transURL = dir.appendingPathComponent(self.directory + "/" + fileName + "_trans.xml")
                     let duraURL = dir.appendingPathComponent(self.directory + "/dura.txt")
                     let offsetURL = dir.appendingPathComponent(self.directory + "/" + fileName + "_offset.txt")
-                    let depthBufferURL = dir.appendingPathComponent(self.directory + "/" + fileName + "_depthBuffer.bin")
-                    let colorJpgURL = dir.appendingPathComponent(directory + "/" + fileName + "_color.jpeg")
-
+//                    let depthBufferURL = dir.appendingPathComponent(self.directory + "/" + fileName + "_depthBuffer.bin")
+//                    let colorJpgURL = dir.appendingPathComponent(self.directory + "/" + fileName + "_color.jpeg")
+//                    let imuURL = dir.appendingPathComponent(self.directory + "/" + fileName + "_imu.xml")
+                    
                     //writing
                     do {
-                        try depthBuffer.write(to: depthBufferURL)
-                        try uiImageColor.jpegData(compressionQuality: 0.0)!.write(to: colorJpgURL)
+//                        try depthBuffer.write(to: depthBufferURL)
+//                        try uiImageColor.jpegData(compressionQuality: 0.0)!.write(to: colorJpgURL)
                         (cameraIntrinsics as NSArray).write(to: intriURL, atomically: false)
                         (cameraTransform as NSArray).write(to: transURL, atomically: false)
                         try exposureDuration.write(to: duraURL, atomically: false, encoding: .utf8)
                         try exposureOffset.write(to: offsetURL, atomically: false, encoding: .utf8)
+//                        (imu as NSArray).write(to: imuURL, atomically: false)
                     }
                     catch {/* error handling here */}
                 }
